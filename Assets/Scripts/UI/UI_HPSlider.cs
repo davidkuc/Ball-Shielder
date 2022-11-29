1ba3f40a9528f879ea954bea89f662a0e2a2c001
davidkuc <91789757+davidkuc@users.noreply.github.com>
2022-11-29 17:36:22 +0100

    Hp color updates with current HP!

--- Assets/Scripts/UI/UI_HPSlider.cs
+++ Assets/Scripts/UI/UI_HPSlider.cs
@@ -6,19 +6,35 @@ namespace BallShielder
 {
     public class UI_HPSlider : Debuggable
     {
+        [SerializeField] private Color color_MaxHP;
+        [SerializeField] private Color color_MinHP;
+
         private Player player;
         private GameManager gameManager;
         private SignalBus signalBus;
 
         private Slider hpSlider;
+        private Image sliderImage;
 
-        private void Awake() => hpSlider = GetComponent<Slider>();
+        private void Awake()
+        {
+            hpSlider = GetComponent<Slider>();
+            sliderImage = transform.Find("Fill Area").Find("Fill").GetComponent<Image>();
+        }
 
         private void Start() => SetupHPSlider();
 
-        private void OnEnable() => signalBus.Subscribe<PlayerDamagedSignal>(UpdateHPSlider);
+        private void OnEnable()
+        {
+            signalBus.Subscribe<PlayerDamagedSignal>(UpdateHPSlider);
+            hpSlider.onValueChanged.AddListener(delegate { UpdateColor(); });
+        }
 
-        private void OnDisable() => signalBus.Unsubscribe<PlayerDamagedSignal>(UpdateHPSlider);
+        private void OnDisable()
+        {
+            signalBus.Unsubscribe<PlayerDamagedSignal>(UpdateHPSlider);
+            hpSlider.onValueChanged.RemoveListener(delegate { UpdateColor(); });
+        }
 
         [Inject]
         public void Setup(GameManager gameManager, Player player, SignalBus signalBus)
@@ -36,10 +52,12 @@ namespace BallShielder
             UpdateHPSlider();
         }
 
-        public void UpdateHPSlider()
+        private void UpdateHPSlider()
         {
             PrintDebugLog("Updated HP!");
             hpSlider.value = player.Hp.CurrentHP;
         }
+
+        private void UpdateColor() => sliderImage.color = Color.Lerp(color_MinHP, color_MaxHP, hpSlider.value/ 10);
     }
 }

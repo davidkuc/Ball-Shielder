58e0a28ed51c4d18235934a9e742eb3d789049af
davidkuc <91789757+davidkuc@users.noreply.github.com>
2022-11-30 13:45:02 +0100

    Added text popup tween

---
+++ Assets/Scripts/UI/UI_TextPopup.cs
@@ -0,0 +1,64 @@
+using TMPro;
+using UnityEngine;
+
+namespace BallShielder
+{
+    public class UI_TextPopup : MonoBehaviour
+    {
+        private Vector3 defaultScale;
+        private Vector3 defaultPosition;
+
+        [SerializeField] private float x_MoveTime;
+        [SerializeField] private float x_MoveDistance;
+        [Space]
+        [SerializeField] private float y_MoveTime;
+        [SerializeField] private float y_MoveDistance;
+        [SerializeField] private float y_MoveUpDelay;
+        [Space]
+        [SerializeField] private Vector3 targetScale;
+        [SerializeField] private float scaleTime;
+
+        private TextMeshProUGUI tmp_Text;
+
+        private void Awake()
+        {
+            tmp_Text = transform.Find("text").GetComponent<TextMeshProUGUI>();
+
+            defaultScale = transform.localScale;
+            defaultPosition = transform.position;
+        }
+
+        public void SetPopupText(string text) => this.tmp_Text.text = text;
+
+        [ContextMenu("Animate MoveX")]
+        public void Animation_MoveX() => LeanTween.moveX(gameObject, transform.position.x + x_MoveDistance, x_MoveTime);
+
+        [ContextMenu("Animate MoveX SetEaseOutCirc")]
+        public void Animation_MoveX_SetEaseOutCirc() => LeanTween.moveX(gameObject, transform.position.x + x_MoveDistance, x_MoveTime)
+            .setEaseOutCirc();
+
+        [ContextMenu("Animate MoveX SetEaseOutElastic")]
+        public void Animation_MoveX_SetEaseOutElastic() => LeanTween.moveX(gameObject, transform.position.x + x_MoveDistance, x_MoveTime)
+            .setEaseOutElastic();
+
+        [ContextMenu("Animate Test")]
+        public void Animation_Test()
+        {
+            LeanTween.moveX(gameObject, transform.position.x + x_MoveDistance, x_MoveTime).setEaseOutElastic();
+            LeanTween.scale(gameObject, targetScale, scaleTime).setEaseOutQuart().setLoopPingPong();
+            LeanTween.moveY(gameObject, transform.position.y + y_MoveDistance, y_MoveTime).setEaseInElastic().setDelay(y_MoveUpDelay);
+        }
+
+        [ContextMenu("Reset")]
+        public void ResetAnim()
+        {
+            LeanTween.cancel(gameObject);
+            DefaultScale();
+            DefaultPosition();
+        }
+
+        private void DefaultScale() => transform.localScale = defaultScale;
+
+        private void DefaultPosition() => transform.position = defaultPosition;
+    }
+}
